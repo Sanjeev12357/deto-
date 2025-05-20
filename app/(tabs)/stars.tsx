522f905630fcd9ddf6e97d2d1379e96250cc04ff
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2025-05-20 20:09:18 +0530

    first commit

---
+++ app/(tabs)/stars.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const stars = () => {
+  return (
+    <View>
+      <Text>stars</Text>
+    </View>
+  )
+}
+
+export default stars
